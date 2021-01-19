e4ea7d285191a654891ecacc77840763a5c1fd4b
su-mahesh <maheshkangude@gmail.com>
2021-01-19 19:21:23 +0530

    REFACTORED

--- src/main/java/com/bridgelabz/MoodAnanlyser.java
+++ src/main/java/com/bridgelabz/MoodAnanlyser.java
@@ -3,6 +3,11 @@ package com.bridgelabz;
 import java.util.Locale;
 
 public class MoodAnanlyser {
+
+    MoodAnanlyser(){}
+    MoodAnanlyser(String message){
+
+    }
     public String analyseMood(String message)  {
         if (message.toLowerCase().contains("sad"))
             return "SAD";
