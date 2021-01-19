0c78f88073f6b1343747111fd2d9f7e115902744
su-mahesh <maheshkangude@gmail.com>
2021-01-19 18:51:38 +0530

    ADDED: given any mood should return HAPPY

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -6,10 +6,16 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 
     @Test
-    public void givenSad_ShouldReturnSad(){
+    public void givenMessageSad_ShouldReturnSad(){
         MoodAnanlyser moodAnanlyser = new MoodAnanlyser();
         String mood = moodAnanlyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("SAD", mood);
     }
-}
 
+    @Test
+    public void givenMessageAnyMood_ShouldReturnHappy(){
+        MoodAnanlyser moodAnanlyser = new MoodAnanlyser();
+        String mood = moodAnanlyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
